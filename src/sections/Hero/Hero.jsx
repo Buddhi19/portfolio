3f0ff853dda06182d040bba8c1e0e32b89646164
Buddhi19 <wijenayakebuddhi34802@gmail.com>
2025-10-13 01:29:45 +0530

    feat: Enhance responsive design across multiple sections with media queries and improved styles

--- src/sections/Hero/Hero.jsx
+++ src/sections/Hero/Hero.jsx
@@ -1,8 +1,6 @@
 import styles from './HeroStyles.module.css';
 // import FallingStars from './FallingStars';
 import heroImg from '../../assets/dp.png';
-import sun from '../../assets/sun.svg';
-import moon from '../../assets/moon.svg';
 import twitterLight from '../../assets/twitter-light.svg';
 import twitterDark from '../../assets/twitter-dark.svg';
 import githubLight from '../../assets/github-light.svg';
@@ -13,9 +11,8 @@ import CV from '../../assets/cv.pdf';
 import { useTheme } from '../../common/ThemeContext';
 
 function Hero() {
-  const { theme, toggleTheme } = useTheme();
+  const { theme } = useTheme();
 
-  const themeIcon = theme === 'light' ? moon : sun;
   const twitterIcon = theme === 'light' ? twitterLight : twitterDark;
   const githubIcon = theme === 'light' ? githubLight : githubDark;
   const linkedinIcon = theme === 'light' ? linkedinLight : linkedinDark;
@@ -24,51 +21,52 @@ function Hero() {
     <section id="hero" className={styles.container}>
       {/* <FallingStars theme={theme}/> */}
       <div className={styles.colorModeContainer}>
-        <img
-          src={heroImg}
-          className={styles.hero}
-          alt="Profile picture of Buddhi Wijenayake"
-        />
-        {/* <img
-          className={styles.colorMode}
-          src={themeIcon}
-          alt="Color mode icon"
-          onClick={toggleTheme}
-        /> */}
+        <img src={heroImg} className={styles.hero} alt="Profile picture of Buddhi Wijenayake" />
       </div>
       <div className={styles.info}>
         <h1>
           Buddhi
           Wijenayake
         </h1>
-        <h2>Electrical and electronic engineering student
-        </h2>
-        <span>
-          <a href="https://twitter.com/Buddhi1100" target="_blank">
+        <h2>Electrical and electronic engineering student</h2>
+        <div className={styles.socials}>
+          <a href="https://twitter.com/Buddhi1100" target="_blank" rel="noreferrer">
             <img src={twitterIcon} alt="Twitter icon" />
           </a>
-          <a href="https://github.com/Buddhi19" target="_blank">
-            <img src={githubIcon} alt="Github icon" />
+          <a href="https://github.com/Buddhi19" target="_blank" rel="noreferrer">
+            <img src={githubIcon} alt="GitHub icon" />
           </a>
-          <a href="https://www.linkedin.com/in/buddhi-wijenayake-696707321/" target="_blank">
-            <img src={linkedinIcon} alt="Linkedin icon" />
+          <a href="https://www.linkedin.com/in/buddhi-wijenayake-696707321/" target="_blank" rel="noreferrer">
+            <img src={linkedinIcon} alt="LinkedIn icon" />
           </a>
-          <a href="https://scholar.google.com/citations?user=HDFMos8AAAAJ&hl=en" target="_blank">
-            <img src="https://upload.wikimedia.org/wikipedia/commons/c/c7/Google_Scholar_logo.svg" alt="Google Scholar icon" style={{width: '24px', height: '24px'}} />
+          <a href="https://scholar.google.com/citations?user=HDFMos8AAAAJ&hl=en" target="_blank" rel="noreferrer">
+            <img
+              src="https://upload.wikimedia.org/wikipedia/commons/c/c7/Google_Scholar_logo.svg"
+              alt="Google Scholar icon"
+              style={{ width: '24px', height: '24px' }}
+            />
           </a>
-          <a href="https://www.youtube.com/channel/UC0hLpWIMpegCzoTwPbYtQ6g" target="_blank">
-            <img src="https://upload.wikimedia.org/wikipedia/commons/4/42/YouTube_icon_%282013-2017%29.png" alt="YouTube icon" style={{width: '28px', height: '24px'}} />
+          <a href="https://www.youtube.com/channel/UC0hLpWIMpegCzoTwPbYtQ6g" target="_blank" rel="noreferrer">
+            <img
+              src="https://upload.wikimedia.org/wikipedia/commons/4/42/YouTube_icon_%282013-2017%29.png"
+              alt="YouTube icon"
+              style={{ width: '28px', height: '24px' }}
+            />
           </a>
-        </span>
+        </div>
         <p className={styles.description}>
-        I am a final year Electrical and Electronic Engineering student at the <a href="https://eng.pdn.ac.lk/" target="_blank" style={{color: 'blue', textDecoration: 'none'}}> University of Peradeniya</a>, Sri Lanka.
-        <br/>
-        <br/>
-        <br/>
-        My research interests include Computer Vision, Remote Sensing, Signal Processing, and Deep Learning, with a focus on Semantic Change Detection and data-driven approaches for real-world applications.   
+          I am a final year Electrical and Electronic Engineering student at the{' '}
+          <a href="https://eng.pdn.ac.lk/" target="_blank" rel="noreferrer">
+            University of Peradeniya
+          </a>
+          , Sri Lanka.
         </p>
-        <a href={CV} view>
-          <button className="hover">Resume</button>
+        <p className={styles.description}>
+          My research interests include Computer Vision, Remote Sensing, Signal Processing, and Deep Learning, with a
+          focus on Semantic Change Detection and data-driven approaches for real-world applications.
+        </p>
+        <a className={styles.resumeLink} href={CV} target="_blank" rel="noreferrer">
+          <button className={styles.resumeButton}>Resume</button>
         </a>
       </div>
     </section>
